a7f0fad386598508a4c092109b758dc9c0ed054e
Ayaan <kaifullah7921@gmail.com>
2022-10-11 01:41:28 +0530

    set up mantine CRA project. Stuled usnign props only. Now psdd the keyword

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MantineProvider } from "@mantine/core";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{
+        colorScheme: "dark",
+        colors: {
+            brand: ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82','#AD1374' ],
+          },
+          primaryColor: 'brand',
+    }} >
+        <App />
+    </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
