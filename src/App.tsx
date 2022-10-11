6a26d7d0133db8402a6b4721546985f5fbf02073
Ayaan <kaifullah7921@gmail.com>
2022-10-12 03:11:54 +0530

    styling support for editor toolbar, and general layout of 2 major components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import "./App.css";
-import {  Grid } from "@mantine/core";
+import { Grid } from "@mantine/core";
 import NewsHandler from "./components/newsHandler/NewsHandler";
 import Editor from "./components/textEditor/Editor";
+import { createStyles } from "@mantine/core";
 
+const useStyles = createStyles((theme) => ({
+    editor: {
+        backgroundColor:
+            theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+        margin: "auto",
+    },
+    header: {
+        padding: theme.spacing.lg,
+        marginBottom: theme.spacing.md,
+        borderBottom: `3px solid ${theme.colors.gray[2]}`,
+    },
+}));
 function App() {
+    const { classes } = useStyles();
     return (
         <div className="App">
-            <div>Docustack Assignment</div>
+            <div className={classes.header}>Docustack Assignment</div>
             <Grid>
                 <Grid.Col span={6}>
-                    <Editor />
+                    <div className={classes.editor}>
+                        <Editor />
+                    </div>
                 </Grid.Col>
                 <Grid.Col span={6}>
                     <NewsHandler />
