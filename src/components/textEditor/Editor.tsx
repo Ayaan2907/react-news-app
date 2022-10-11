59b3fa6062f6fb8375e4d8faebaf70924a620b02
Ayaan <kaifullah7921@gmail.com>
2022-10-12 00:39:39 +0530

    lexical editor initialised, putting the plugins here in the editor. Now handeling the plugins

--- src/components/textEditor/Editor.tsx
+++ src/components/textEditor/Editor.tsx
@@ -1,72 +1,55 @@
-import {$getRoot, $getSelection} from 'lexical';
-import {useEffect} from 'react';
+import { $getRoot, $getSelection } from "lexical";
 
-import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
-import {ContentEditable} from '@lexical/react/LexicalContentEditable';
-import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
-import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { TRANSFORMERS } from "@lexical/markdown";
 
-const theme = {
-  // Theme styling goes here
+import ToolbarPlugin from "./plugins/ToolbarPlugin";
+import CodeHighlightPlugin from "./plugins/CodeHighlightPlugin";
+import AutoLinkPlugin from "./plugins/AutoLinkPlugin";
+// import TreeViewPlugin from "./plugins/TreeViewPlugin";
+// import ListMaxIndentLevelPlugin from "./plugins/ListMaxIndentLevelPlugin";
+
+import editorConfig from "./EditorConfig";
+import { Text } from "@mantine/core";
+import { useState } from "react";
+
+const Placeholder = () => {
+    return <Text className="editor-placeholder">Enter some rich text...</Text>;
 };
 
-// When the editor changes, you can get notified via the
-// LexicalOnChangePlugin!
-function onChange(editorState : any) {
-  editorState.read(() => {
-    // Read the contents of the EditorState here.
-    const root = $getRoot();
-    const selection = $getSelection();
-
-    console.log(root, selection);
-  });
+export default function Editor() {
+    const [value, setValue] = useState("");
+    const [editor, setEditor] = useState(null);
+    // TODO: add a way to dispatch the addText command to insert the news text into editor
+    return (
+        <LexicalComposer initialConfig={editorConfig}>
+            <div className="editor-container">
+                <ToolbarPlugin />
+                <div className="editor-inner">
+                    <RichTextPlugin
+                        contentEditable={
+                            <ContentEditable className="editor-input" />
+                        }
+                        placeholder={<Placeholder />}
+                    />
+                    <HistoryPlugin />
+                    <AutoFocusPlugin />
+                    <CodeHighlightPlugin />
+                    <ListPlugin />
+                    <LinkPlugin />
+                    <AutoLinkPlugin />
+                    <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+                    {/* <TreeViewPlugin /> */}
+                    {/* <ListMaxIndentLevelPlugin maxDepth={7} /> */}
+                </div>
+            </div>
+        </LexicalComposer>
+    );
 }
-
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
-function MyCustomAutoFocusPlugin() {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    // Focus the editor when the effect fires!
-    editor.focus();
-  }, [editor]);
-
-  return null;
-}
-
-// Catch any errors that occur during Lexical updates and log them
-// or throw them as needed. If you don't throw them, Lexical will
-// try to recover gracefully without losing user data.
-function onError(error : any) {
-  console.error(error);
-}
-
-function Editor() {
-  const initialConfig = {
-    namespace: 'MyEditor', 
-    theme,
-    onError,
-  };
-
-  return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>Enter some text...</div>}
-      />
-      <OnChangePlugin onChange={onChange} />
-      <HistoryPlugin />
-      <MyCustomAutoFocusPlugin />
-    </LexicalComposer>
-  );
-}
-
-export default Editor;
-
-
-
---
+++ src/components/textEditor/EditorConfig.ts
@@ -0,0 +1,29 @@
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
+
+import ExampleTheme from "./theme/PrimaryTheme";
+
+const editorConfig = {
+    theme: ExampleTheme,
+    onError(error: any) {
+        throw error;
+    },
+    nodes: [
+        HeadingNode,
+        ListNode,
+        ListItemNode,
+        QuoteNode,
+        CodeNode,
+        CodeHighlightNode,
+        TableNode,
+        TableCellNode,
+        TableRowNode,
+        AutoLinkNode,
+        LinkNode,
+    ],
+};
+
+export default editorConfig;
